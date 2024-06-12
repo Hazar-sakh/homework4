14c3e87d1b8db54ee48487324631ec2889dc1d94
Nikita <niknakels@gmail.com>
2024-06-12 14:20:59 +1100

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,7 +1,7 @@
-my_string = input('')
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
+my_string = input('') # запрос ввода с клавиатуры значения 
+print(len(my_string)) # вывод количества символов
+print(my_string.upper()) # перевод текста в верхний регистр
+print(my_string.lower()) # перевод текста в нижний регистр
+print(my_string.replace(' ', '')) # замена символов (вместо пробела - ничего)
+print(my_string[0]) # вывод первого символа
+print(my_string[-1]) # вывод последнего символа
